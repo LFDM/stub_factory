306f5949dc868ff5eca1115f71894dfc181bf6e0
LFDM <1986gh@gmail.com>
2013-09-12 11:32:47 +0200

    Imports files from LLT.

---
+++ spec/lib/stub_factory_spec.rb
@@ -0,0 +1,90 @@
+require 'spec_helper'
+require_relative '../../lib/stub_factory.rb'
+
+
+describe StubFactory do
+  before :all do
+    # the attr reader is set for convenience
+    class A; attr_reader :test; end
+  end
+
+  describe "#new_stub" do
+    it "returns a new instance" do
+      A.new_stub.should be_an_instance_of A
+    end
+
+    it "takes variable hash and turns it to instance variables" do
+      o = A.new_stub(vars: { test: 1 })
+      o.test.should == 1
+    end
+
+    context "when a default template exists" do
+      module StubFactory
+        def stub_template_for_a
+          { test: 1 }
+        end
+      end
+
+      it "instantiates with default values of a template, deriving from class name" do
+
+        A.new_stub.test.should == 1
+      end
+
+      it "overrides template when instantiated with vars hash" do
+        o = A.new_stub(vars: { test: 2 })
+        o.test.should_not == 1
+        o.test.should     == 2
+      end
+
+      it "disables template set to nil" do
+        o = A.new_stub(template: nil)
+        o.test.should be_nil
+      end
+    end
+
+    context "when a custom template exists" do
+      module StubFactory
+        def stub_template_for_esse
+          { test: 12 }
+        end
+      end
+
+      it "takes custom template as argument" do
+        o = A.new_stub(template: :esse)
+        o.test.should == 12
+      end
+
+      it "custom templates can be overridden" do
+        o = A.new_stub(template: :esse, vars: { test: 1 })
+        o.test.should == 1
+      end
+    end
+
+    context "when methods need to be overridden" do
+      class A
+        def test_method
+          12
+        end
+      end
+
+      it "should leave methods intact without methods hash" do
+        A.new_stub.test_method.should == 12
+      end
+
+      it "overrides methods with methods hash" do
+        o = A.new_stub(methods: { test_method: 1 })
+        o.test_method.should == 1
+      end
+
+      it "other instances' methods are not overridden by methods hash" do
+        a = A.new_stub(methods: { test_method: 1 })
+        b = A.new_stub(methods: { test_method: 2 })
+        c = A.new_stub
+
+        a.test_method.should == 1
+        b.test_method.should == 2
+        c.test_method.should == 12
+      end
+    end
+  end
+end
